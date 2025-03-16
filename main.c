3668ac4b03b26f634d78c308bd5a2292eca876a2
Marcos Irenos de Souza <marcosaurelio.irenos@gmail.com>
2025-03-16 18:06:31 -0300

    Update main
    Tentativa número 1 de resolver o problema proposto. Usei valores fixos por achar mais fácil primeiro quebrar o problema e resolver em partes. Consegui colocar a verificação de rodadas e jogadores, coloquei um loop para cada um (rodadas e jogadores) para cada rodada um loop sobre a quantidade de jogadores. Criei arrays para guardar a pontuação onde cada posição do array representa um jogador e uma variável (contador) para atribuir pontos para esse array que zera depois que todos os jogadores tiveram seus pontos. No fim adicionei uma verificação de ganhador que simplesmente compara a pontuação atual (i) no loop com a maior pontuação registrada, se a pontuação for maior ou igual então a nova maior pontuação ganha o novo valor. No fim apenas o print para dizer quem ganhou. Detalhe: Sim, tem um monte de prints mostrando jogador atual, rodada e pontos da rodada para aquele jogador, porém tem um erro de lógica, isso não vai ser utilizado então pode jogar fora. Detalhe 2: Sim, tem muitos loops

--- main.c
+++ main.c
@@ -1,8 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
+int main(void)
 {
-    printf("Hello world!\n");
-    return 0;
+    int jogadores;
+    int rodadas;
+    int pontosRodada;
+    int pontuacaoGeral[] = {1, 1, 1, 1, 2, 2, 2, 3, 3};
+    int qtd_pontos = 0;
+    int guardaPontuacao[] = {0, 0, 0};
+    int contador = 0;
+    jogadores = 3;
+    rodadas = 3;
+
+    if (1 <= jogadores && rodadas <= 500)
+    {
+        for (int i = 1; i <= rodadas; i++)
+        {
+            for (int j = 1; j <= jogadores; j++)
+            {
+                guardaPontuacao[contador] = guardaPontuacao[contador] + pontuacaoGeral[qtd_pontos];
+                printf("Rodada: %d, jogador %d, quantidade de ponto na rodada: %d\n", i, j, pontuacaoGeral[i]);
+                contador++;
+                qtd_pontos++;
+            }
+            contador = 0;
+        }
+    }
+
+    int maiorQtdDePontosTotal = 0;
+    int maiorQtdPontosAtual = 0;
+    int indexJogadorGanhador = 0;
+
+    for (int i = 0; i < 3; i++)
+    {
+        maiorQtdPontosAtual = guardaPontuacao[i];
+        if (maiorQtdPontosAtual >= maiorQtdDePontosTotal)
+        {
+            maiorQtdDePontosTotal = maiorQtdPontosAtual;
+            indexJogadorGanhador = i;
+        }
+    }
+    printf("Jogador ganhador: %d", indexJogadorGanhador + 1);
 }
